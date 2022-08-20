5f941f97fe94bb92285374bd7e6b2750f04fbe56
Charlie <45103062+Jean-4k@users.noreply.github.com>
2022-08-20 13:38:51 +0200

    Bug.

--- bug-sbcl-guix.scm
+++ bug-sbcl-guix.scm
@@ -1,8 +1,13 @@
 (define-module (bug-sbcl-guix)
   #:use-module (guix profiles)
   #:use-module (guix packages)
-  #:use-module (guix git)
+  #:use-module (guix gexp)
+  #:use-module (gnu packages lisp-xyz)
   #:use-module (guix build-system asdf))
+
+
+
+(define %source-dir (dirname (current-filename)))
 
 
 
@@ -10,15 +15,18 @@
   (package
     (name "bug-sbcl-guix")
     (version "1.0.0")
-    (source (git-checkout
-             (url "https://github.com/Jean-4k/bug-sbcl-guix.git")
-	     (branch "main")
-	     (commit "fcc931f7718b25b961183cfd3d6048f1f3d2997d")))
+    (source
+    (local-file %source-dir
+                #:recursive? #t))
     (build-system asdf-build-system/sbcl)
-   (arguments
-    '(#:asd-systems '("bug-sbcl-guix")))
-   (inputs `(("sbcl-dbi" ,sbcl-dbi)))
-   (synopsis "A bug on SBCL with guix.")
-   (description "A bug on SBCL with guix.")
-   (license "none")
-   (home-page "")))
+    (arguments
+     '(#:asd-systems '("bug-sbcl-guix")))
+    (inputs `(("sbcl-dbi" ,sbcl-dbi)))
+    (synopsis "A bug on SBCL with guix.")
+    (description "A bug on SBCL with guix.")
+    (license "none")
+    (home-page "")))
+
+
+
+bug-sbcl-guix
